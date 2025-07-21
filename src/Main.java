6dca2c507ec249ee53aa5a662330cfab63003a6d
adarshfyntune <adarshpingale3114@gmail.com>
2025-07-21 12:56:50 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
             System.out.println("Print Data Sucessfully");
             System.out.println("Print Data Sucessfully");
             //changes done
+               System.out.println("Print Data Sucessfully");
+            System.out.println("Print Data Sucessfully");
 
         }
     }
